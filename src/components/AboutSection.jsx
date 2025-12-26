77b1b8690087f3a7dc079c1d7913ec8a96c9106b
karthikch2630 <karthikchitti55@gmail.com>
2025-12-26 17:01:00 +0530

    Social Media Icons Added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,7 +7,7 @@ export default function AboutSection() {
 
         {/* ================= TEXT CONTENT ================= */}
         <Motion.div
-          initial={{ opacity: 0, y: 24 }}s
+          initial={{ opacity: 0, y: 24 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
           viewport={{ once: true }}
